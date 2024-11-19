ca607578ac33752f57cd1bd4f2e4b18a3bb4c397
James Richardson <76536315+Icyfiremario@users.noreply.github.com>
2024-11-19 11:50:06 -0700

    renamed main to app and commented start function

--- app.py
+++ app.py
@@ -32,5 +32,5 @@ def reset():
     model.reboot()
     return jsonify({"reset": True})
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+#if __name__ == '__main__':
+#    app.run(debug=True, host='0.0.0.0')
